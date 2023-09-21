f09c4d3b978c59d1f2b29d3aa471fd20826a5240
LuisU <87432225+LuisUsca@users.noreply.github.com>
2023-09-21 12:37:34 -0500

    Update Sample size.R

--- Sample size.R
+++ Sample size.R
@@ -6,10 +6,8 @@ library(openxlsx)
 library(tidyr)#spread
 library(irr)# corr intra clas icc
 
-setwd("Documentos/Imarpe/Seguimiento")# path for windows
-setwd("F:/Imarpe/Seguimiento")#path for linux
+setwd("Documentos/Github/Effective_Sample_Size")# path for linux
+setwd("F:/Imarpe/Seguimiento")#path for windows
 # Datos -------------------------------------------------------------------
-data <- read.xlsx(xlsxFile = "BD-Lorna.xlsx", sheet = "Biometrico")
-dato <- dato %>% filter(ANIO %in% (2000:2020)) 
-dato[, 30] <- round(as.numeric(dato[, 30]))# sapply(dato[, 29], as.numeric)
-data_nm <- dato %>% group_by(ANIO, MES, DIA, LABORATORIO, EMBARCACION, ARTE) %>% summarise(n=sum(FREC_ABSOLUTA),m=length(LONGITUD))
+data <- read.xlsx(xlsxFile = "Data/Database.xlsx")
+
